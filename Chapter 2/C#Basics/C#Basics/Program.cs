9cd76c835a4c8a96e78b93e21b10c8891d2be4ab
Shikhar Basnet <shikharbasnet123@gmail.com>
2025-02-16 07:30:59 +0545

    Save progress on Program.cs

--- Chapter 2/C#Basics/C#Basics/Program.cs
+++ Chapter 2/C#Basics/C#Basics/Program.cs
@@ -4,10 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Task1.FirstExercise(); //calling function Task1Exercise() fron Task1.cs
-            Task1.SecondExercise(); //calling function Task2Exercise() fron Task1.cs
-
-            Operators.GetOperatorsExercise();
+            //Task1.FirstExercise(); //calling function Task1Exercise() fron Task1.cs
+            //Task1.SecondExercise(); //calling function Task2Exercise() fron Task1.cs
+            //Operators.GetOperatorsExercise();
+            mod1.Caller();
         }
     }
 }
