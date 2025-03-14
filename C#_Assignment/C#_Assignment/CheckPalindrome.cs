08b5881409d6c424cf19789d7b620c93dcc44b3b
gopichkhadka <134199722+gopichkhadka@users.noreply.github.com>
2025-03-15 02:07:00 +0545

    Add files via upload

---
+++ C#_Assignment/C#_Assignment/CheckPalindrome.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CombinedProject
+{
+    public static class CheckPalindrome
+    {
+        public static void CheckAndPrint()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            string reversed = new string(input.Reverse().ToArray());
+
+            if (input.Equals(reversed, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"{input} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is not a palindrome.");
+            }
+        }
+    }
+}
---
+++ C#_Assignment/C#_Assignment/SecondLargest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CombinedProject
+{
+    public static class SecondLargest
+    {
+        public static void FindAndPrint()
+        {
+            Console.Write("Enter the size of the array: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+
+            Console.WriteLine("Enter the elements of the array:");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int first = int.MinValue, second = int.MinValue;
+
+            foreach (var num in arr)
+            {
+                if (num > first)
+                {
+                    second = first;
+                    first = num;
+                }
+                else if (num > second && num != first)
+                {
+                    second = num;
+                }
+            }
+
+            if (second == int.MinValue)
+            {
+                Console.WriteLine("There is no second largest element.");
+            }
+            else
+            {
+                Console.WriteLine($"The second largest element is: {second}");
+            }
+        }
+    }
+}
