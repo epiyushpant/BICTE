1536e975f0d0ca818db488afede434a99a13dd25
Nima Tsering Tamang <126693666+marooners10@users.noreply.github.com>
2025-03-26 07:53:32 +0545

    Code Assignment by Nima Tsering Tamang

---
+++ c#/FindMissingNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CombinedProject
+{
+    public static class FindMissingNumber
+    {
+        public static void FindAndPrint()
+        {
+            Console.Write("Enter the size of the array: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+
+            Console.WriteLine("Enter the elements of the array:");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int n = size + 1;
+            int total = n * (n + 1) / 2;
+            int sum = 0;
+
+            foreach (var num in arr)
+            {
+                sum += num;
+            }
+
+            int missing = total - sum;
+            Console.WriteLine($"The missing number is: {missing}");
+        }
+    }
+}
---
+++ c#/ReverseString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace CombinedProject
+{
+    public static class ReverseString
+    {
+        public static void ReverseAndPrint()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            string reversed = new string(input.Reverse().ToArray());
+            Console.WriteLine("Reversed string: " + reversed);
+        }
+    }
+}
