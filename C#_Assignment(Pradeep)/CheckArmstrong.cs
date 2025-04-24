0de6730b2a751af508d8d883c90d50c038dc76c1
Pradeep Shah <154973405+Pradeepshah19@users.noreply.github.com>
2025-04-24 13:16:19 +0545

    c# assignment uploaded

---
+++ C#_Assignment(Pradeep)/CheckArmstrong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CombinedProject
+{
+    public static class CheckArmstrong
+    {
+        public static void CheckAndPrint()
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            int original = number;
+            int sum = 0;
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sum += digit * digit * digit;
+                number /= 10;
+            }
+
+            if (sum == original)
+            {
+                Console.WriteLine($"{original} is an Armstrong number.");
+            }
+            else
+            {
+                Console.WriteLine($"{original} is not an Armstrong number.");
+            }
+        }
+    }
+}
---
+++ C#_Assignment(Pradeep)/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CombinedProject
+{
+    public static class FibonacciSeries
+    {
+        public static void PrintSeries()
+        {
+            Console.Write("Enter the number of terms: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci Series:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static int Fibonacci(int num)
+        {
+            if (num == 0) return 0;
+            if (num == 1) return 1;
+            return Fibonacci(num - 1) + Fibonacci(num - 2);
+        }
+    }
+}
